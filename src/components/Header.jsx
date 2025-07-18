61ff31d583f06840ad2d0eb56d17e590ec22294c
Muzaffar Ali <90896689+muzaffar9988@users.noreply.github.com>
2025-07-18 22:10:09 +0530

    create the header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import {FaMoon} from 'react-icons/fa';
 export default function Header() {
   const path = useLocation().pathname;
-  
+
   return (
     <Navbar className='border-b-2'>
       <Link to="/" className='self-center whitespace-normal text-sm sm:text-xl font dark:text-white'>
